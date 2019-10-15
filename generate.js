7b61e0e8dd5275927984895772ae1f031f7fee95
Nicholas Weiner <wnicholas114@gmail.com>
2019-10-15 16:20:13 -0400

    All of the game so far

---
+++ generate.js
@@ -0,0 +1,91 @@
+// module aliases
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Body = Matter.Body,
+    Events = Matter.Events;
+
+// create an engine
+var engine = Engine.create();
+engine.timing.timeScale = 1.2;
+
+// create a renderer
+var render = Render.create({
+    element: document.body,
+    canvas: canvas,
+    options: {
+        width: 800,
+        height: 800},
+    engine: engine
+});
+var shapes = [];
+
+var rand = Math.ceil(Math.random() * 7) + 3;
+
+var ground = Bodies.rectangle(500, 800, 1010, 60, {isStatic: true});
+var fruit = Bodies.circle(400, 50, 5);
+var base = Bodies.rectangle(400, 690, 100, 160, {isStatic: true});
+var mouth = Bodies.rectangle(400, 615, 60, 20, {isStatic: true});
+var teethA = Bodies.polygon(370, 600, 3, 20, {isStatic: true})
+Body.rotate(teethA, 7 * (Math.PI/6));
+var teethB = Bodies.polygon(430, 600, 3, 20, {isStatic: true})
+Body.rotate(teethB, 7 * (Math.PI/6));
+
+shapes = [ground, fruit, base, mouth, teethA, teethB];
+
+for(var i = 0; i < rand; i++) {
+    var randshape = Math.floor(Math.random() * 5);
+
+    var randX = Math.random() * 600 + 100,
+        randY = Math.random() * 300 + 150;
+    
+    var shape;
+
+    switch(randshape){
+
+        // 0 for square
+        // default should never trigger. added here for contingency
+        default:
+        case 0:
+            var side = Math.random() * 200 + 1;
+            shape = Bodies.rectangle(randX, randY, side, side, {isStatic: true});
+            break;
+
+        // 1 for rectangle
+        case 1:
+            var width = Math.random() * 200 + 1 ,
+                height = Math.random() * 200 + 1;
+            shape = Bodies.rectangle(randX, randY, width, height, {isStatic: true});
+            break;
+
+        // 2 for circle
+        case 2:
+            var radius = Math.random() * 100 + 1;
+            shape = Bodies.circle(randX, randY, radius, {isStatic: true});
+            break;
+
+        // 3 and 4 for equalateral or right triangle
+        case 3:
+        case 4:
+            var slope = randshape - 2;
+            var width = Math.random() * 200 + 1,
+                height = Math.random() * 200 + 1;
+            shape = Bodies.trapezoid(randX, randY, width, height, slope, {isStatic: true});
+            break;
+    }
+
+    var rot = Math.random() * 2;
+
+    Body.rotate(shape, Math.PI * rot);
+    shapes[shapes.length] = shape;
+}
+
+// add all of the bodies to the world
+World.add(engine.world, shapes);
+
+// run the engine
+Engine.run(engine);
+
+// run the renderer
+Render.run(render);
---
+++ sketch.js
@@ -0,0 +1,99 @@
+// "global" variables
+var canMove = true;
+
+// module aliases
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Body = Matter.Body,
+    Events = Matter.Events;
+
+// create an engine
+var engine = Engine.create();
+
+// create a renderer
+var render = Render.create({
+    element: document.body,
+    canvas: canvas,
+    options: {
+        width: 800,
+        height: 800},
+    engine: engine
+});
+
+// creates all necessary game objects
+var obst0 = Bodies.rectangle(400, 200, 80, 80, {isStatic: true });
+Body.rotate(obst0, Math.PI/4);
+
+var obst1 = Bodies.rectangle(325, 400, 20, 160, {isStatic: true });
+Body.rotate(obst1, 7 * (Math.PI/4));
+
+var base = Bodies.rectangle(400, 690, 100, 160, {isStatic: true});
+var mouth = Bodies.rectangle(400, 615, 60, 20, {isStatic: true});
+var teethA = Bodies.polygon(370, 600, 3, 20, {isStatic: true})
+Body.rotate(teethA, 7 * (Math.PI/6));
+var teethB = Bodies.polygon(430, 600, 3, 20, {isStatic: true})
+Body.rotate(teethB, 7 * (Math.PI/6));
+var fruit = Bodies.circle(400, 50, 5, {isStatic: true});
+var ground = Bodies.rectangle(500, 800, 1010, 60, {isStatic: true});
+var butWidth = 100;
+var butHeight = 50;
+var button = Bodies.rectangle(700, 100, butWidth, butHeight, {isStatic: true});
+
+// add all of the bodies to the world
+World.add(engine.world, [ground, base, fruit, teethA, teethB, mouth, button, obst0, obst1]);
+
+// run the engine
+Engine.run(engine);
+
+// run the renderer
+Render.run(render);
+
+// activates on hold and drag
+function move(event){
+    if(canMove){
+        var mousex = event.touches[0].clientX;
+
+        Body.translate(mouth, {x: mousex - mouth.position.x, y: 0});
+        Body.translate(teethA, {x: mousex - teethA.position.x - 30, y: 0});
+        Body.translate(teethB, {x: mousex - teethB.position.x + 30, y: 0});
+        Body.translate(base, {x: mousex - base.position.x, y: 0});
+    }
+}
+
+// makes the player unable to move and starts the fruit's physics
+function phase2(){
+    canMove = false;
+    Body.setStatic(fruit, false);
+}
+
+// checks if the start button is pressed
+function startHuh(event){
+    var mousex = event.touches[0].clientX;
+    var mousey = event.touches[0].clientY;
+    var butxrange = [button.position.x - butWidth/2, button.position.x + butWidth/2];
+    var butyrange = [button.position.y - butHeight/2, button.position.y + butHeight/2];
+    if(mousex >= butxrange[0] && mousex <= butxrange[1] && mousey >= butyrange[0] && mousey <= butyrange[1]){
+        phase2();
+    }
+}
+
+// runs collision events (win/lose)
+Events.on(engine, 'collisionStart', function(event) {
+    var pairs = event.pairs;
+    var bodyA = pairs[0].bodyA;
+    var bodyB = pairs[0].bodyB;
+
+    if(bodyA === fruit || bodyB === fruit)
+    {
+        // if one is mouth and the other is fruit, win condition
+        if(bodyA === mouth || bodyB === mouth){
+            console.log("win");
+        }
+        // if one is floor and the other is floor, lose
+        if(bodyA === ground || bodyB === ground){
+            console.log("lose");
+        }
+    }
+});
