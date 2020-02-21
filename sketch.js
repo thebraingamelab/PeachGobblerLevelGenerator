23ae2acf86cd159859e8950ef68287d2f1beb50f
Nicholas Weiner <wnicholas114@gmail.com>
2020-02-21 13:38:33 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -133,6 +133,7 @@ function decode(shapesText) {
             case 2:
                 shape = Bodies.circle(parse[i].xpos, parse[i].ypos, parse[i].properties.radius, { isStatic: true });
                 break;
+            // possibly remove/revise these to deal with phasing issue
             case 3:
             case 4:
                 shape = Bodies.trapezoid(parse[i].xpos, parse[i].ypos, parse[i].properties.width, parse[i].properties.height, parse[i].properties.slope, { isStatic: true });
